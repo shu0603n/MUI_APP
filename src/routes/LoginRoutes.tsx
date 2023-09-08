24c933e4406187e93852dd3fe210c172fce859a7
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-09 01:35:47 +0900

    LoginRoutes.tsx を更新

--- src/routes/LoginRoutes.tsx
+++ src/routes/LoginRoutes.tsx
@@ -16,7 +16,7 @@ const AuthCodeVerification = Loadable(lazy(() => import('pages/auth/code-verific
 // ==============================|| AUTH ROUTING ||============================== //
 
 const LoginRoutes = {
-  path: 'xs739875.xsrv.jp',
+  path: '/',
   children: [
     {
       path: '/',
