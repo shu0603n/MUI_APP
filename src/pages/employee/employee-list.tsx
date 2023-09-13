163262a433bb30c30286edfa8f6a86d4562f2ddd
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-13 13:29:25 +0900

    社員一覧Viewアクション変更

--- src/pages/employee/employee-list.tsx
+++ src/pages/employee/employee-list.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useMemo, useState, FC, Fragment, MouseEvent } from 'react';
-
+import { useEffect, useMemo, useState, Fragment, MouseEvent } from 'react';
+import { useNavigate } from 'react-router';
 // material-ui
 import { alpha, useTheme } from '@mui/material/styles';
 import {
@@ -50,14 +50,13 @@ import {
 } from 'components/third-party/ReactTable';
 
 import AddCustomer from 'sections/apps/employee/AddCustomer';
-import CustomerView from 'sections/apps/employee/CustomerView';
 import AlertCustomerDelete from 'sections/apps/employee/AlertCustomerDelete';
 
 import makeData from 'data/react-table';
 import { renderFilterTypes, GlobalFilter } from 'utils/react-table';
 
 // assets
-import { CloseOutlined, PlusOutlined, EyeTwoTone, EditTwoTone, DeleteTwoTone } from '@ant-design/icons';
+import { PlusOutlined, EyeTwoTone, EditTwoTone, DeleteTwoTone } from '@ant-design/icons';
 
 const avatarImage = require.context('assets/images/users', true);
 
@@ -67,11 +66,10 @@ interface Props {
   columns: Column[];
   data: [];
   handleAdd: () => void;
-  renderRowSubComponent: FC<any>;
   getHeaderProps: (column: HeaderGroup) => {};
 }
 
-function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeaderProps }: Props) {
+function ReactTable({ columns, data, handleAdd, getHeaderProps }: Props) {
   const theme = useTheme();
   const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -85,12 +83,11 @@ function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeader
     prepareRow,
     setHiddenColumns,
     allColumns,
-    visibleColumns,
     rows,
     page,
     gotoPage,
     setPageSize,
-    state: { globalFilter, selectedRowIds, pageIndex, pageSize, expanded },
+    state: { globalFilter, selectedRowIds, pageIndex, pageSize },
     preGlobalFilteredRows,
     setGlobalFilter,
     setSortBy,
@@ -120,7 +117,7 @@ function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeader
   }, [matchDownSM]);
 
   return (
-    <Fragment>
+    <>
       <TableRowSelection selected={Object.keys(selectedRowIds).length} />
       <Stack spacing={3}>
         <Stack
@@ -162,7 +159,6 @@ function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeader
           <TableBody {...getTableBodyProps()}>
             {page.map((row: Row, i: number) => {
               prepareRow(row);
-              const rowProps = row.getRowProps();
 
               return (
                 <Fragment key={i}>
@@ -177,7 +173,6 @@ function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeader
                       <TableCell {...cell.getCellProps([{ className: cell.column.className }])}>{cell.render('Cell')}</TableCell>
                     ))}
                   </TableRow>
-                  {row.isExpanded && renderRowSubComponent({ row, rowProps, visibleColumns, expanded })}
                 </Fragment>
               );
             })}
@@ -189,102 +184,17 @@ function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeader
           </TableBody>
         </Table>
       </Stack>
-    </Fragment>
+    </>
   );
 }
 
 // ==============================|| CUSTOMER - LIST ||============================== //
 
 const EmployeeList = () => {
-  // const [dataList, setDataList] = useState([]);
-  // const [loading, setLoading] = useState(true); // ローディング状態を管理
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:8080/');
-  //       if (!response.ok) {
-  //         throw new Error('データの取得に失敗しました');
-  //       }
-  //       const data = await response.json();
-  //       setDataList(data);
-  //       setLoading(false); // データ取得が完了したらローディングを終了
-  //     } catch (error) {
-  //       console.error('エラー:', error);
-  //       setLoading(false); // エラーが発生した場合もローディングを終了
-  //       // エラーが発生したらエラー画面を表示
-  //       return <div>ERROR...</div>; // あるいは適切なローディングコンポーネントを使用
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-  // console.log(dataList);
-
   const theme = useTheme();
+  const navigation = useNavigate();
 
-  const data = useMemo(() => makeData(0), []);
-  data.push({
-    id: 1,
-    firstName: 'ヤマダ',
-    lastName: 'タカシ',
-    email: 'dsghyjyjuku@gmail.com',
-    age: 29,
-    role: '課長',
-    visits: 7772,
-    progress: 53,
-    status: '0',
-    orderStatus: '正社員',
-    contact: '08023654785',
-    country: 'システムエンジニア',
-    address: '北海道千歳市1-5-9',
-    fatherName: '山田 隆',
-    about: 'Bis pomozfo ar alo me fudtotka rah nuljal gegaseg gijvaruf pemjemu uzewoltif pi ohimu okes toren ahhoj gihka.',
-    avatar: 4,
-    skills: ['ES6', 'Javascript', 'UI Design', 'React', 'Web App'],
-    time: 'システムエンジニア'
-  });
-  data.push({
-    id: 2,
-    firstName: 'スズキ',
-    lastName: 'ユウヤ',
-    email: 'shjghiewfwf@gmail.com',
-    age: 24,
-    role: '主任',
-    visits: 7772,
-    progress: 53,
-    status: '2',
-    orderStatus: '派遣社員',
-    contact: '09035698545',
-    country: 'コールセンター',
-    address: '北海道江別市1-5-9',
-    fatherName: '鈴木 裕也',
-    about: 'Bis pomozfo ar alo me fudtotka rah nuljal gegaseg gijvaruf pemjemu uzewoltif pi ohimu okes toren ahhoj gihka.',
-    avatar: 2,
-    skills: ['ES6', 'Javascript', 'UI Design', 'React', 'Web App'],
-    time: 'システムエンジニア'
-  });
-  data.push({
-    id: 3,
-    firstName: 'ナカムラ',
-    lastName: 'サオリ',
-    email: 'fdsgrhjuki@gmail.com',
-    age: 21,
-    role: '主任',
-    visits: 7772,
-    progress: 53,
-    status: '1',
-    orderStatus: 'フリーランス',
-    contact: '08054589856',
-    country: 'コールセンター',
-    address: '北海道札幌市中央区1-5-9',
-    fatherName: '中村 早織',
-    about: 'Bis pomozfo ar alo me fudtotka rah nuljal gegaseg gijvaruf pemjemu uzewoltif pi ohimu okes toren ahhoj gihka.',
-    avatar: 2,
-    skills: ['ES6', 'Javascript', 'UI Design', 'React', 'Web App'],
-    time: 'システムエンジニア'
-  });
+  const data = useMemo(() => makeData(200), []);
   const [open, setOpen] = useState<boolean>(false);
   const [customer, setCustomer] = useState<any>(null);
   const [customerDeleteId, setCustomerDeleteId] = useState<any>('');
@@ -311,7 +221,7 @@ const EmployeeList = () => {
         disableSortBy: true
       },
       {
-        Header: '社員ID',
+        Header: '顧客ID',
         accessor: 'id',
         className: 'cell-center'
       },
@@ -361,11 +271,11 @@ const EmployeeList = () => {
         accessor: 'status',
         Cell: ({ value }: { value: string }) => {
           switch (value) {
-            case '0':
+            case 'Complicated':
               return <Chip color="error" label="休" size="small" variant="light" />;
-            case '1':
+            case 'Relationship':
               return <Chip color="success" label="退勤" size="small" variant="light" />;
-            case '2':
+            case 'Single':
             default:
               return <Chip color="info" label="出勤" size="small" variant="light" />;
           }
@@ -376,25 +286,14 @@ const EmployeeList = () => {
         className: 'cell-center',
         disableSortBy: true,
         Cell: ({ row }: { row: Row<{}> }) => {
-          const collapseIcon = row.isExpanded ? (
-            <CloseOutlined style={{ color: theme.palette.error.main }} />
-          ) : (
-            <EyeTwoTone twoToneColor={theme.palette.secondary.main} />
-          );
           return (
             <Stack direction="row" alignItems="center" justifyContent="center" spacing={0}>
-              <Tooltip title="View">
-                <IconButton
-                  color="secondary"
-                  onClick={(e: MouseEvent<HTMLButtonElement>) => {
-                    e.stopPropagation();
-                    row.toggleRowExpanded();
-                  }}
-                >
-                  {collapseIcon}
+              <Tooltip title="表示">
+                <IconButton color="secondary" onClick={() => navigation(`/employee/employee-detail/basic`)}>
+                  <EyeTwoTone twoToneColor={theme.palette.secondary.main} />
                 </IconButton>
               </Tooltip>
-              <Tooltip title="Edit">
+              <Tooltip title="編集">
                 <IconButton
                   color="primary"
                   onClick={(e: MouseEvent<HTMLButtonElement>) => {
@@ -406,7 +305,7 @@ const EmployeeList = () => {
                   <EditTwoTone twoToneColor={theme.palette.primary.main} />
                 </IconButton>
               </Tooltip>
-              <Tooltip title="Delete">
+              <Tooltip title="削除">
                 <IconButton
                   color="error"
                   onClick={(e: MouseEvent<HTMLButtonElement>) => {
@@ -427,11 +326,6 @@ const EmployeeList = () => {
     [theme]
   );
 
-  const renderRowSubComponent = useCallback(({ row }: { row: Row<{}> }) => <CustomerView data={data[Number(row.id)]} />, [data]);
-  // if (loading) {
-  //   // ローディング中の場合、ローディングスピナーやメッセージを表示
-  //   return <div>Loading...</div>; // あるいは適切なローディングコンポーネントを使用
-  // }
   return (
     <MainCard content={false}>
       <ScrollX>
@@ -439,7 +333,6 @@ const EmployeeList = () => {
           columns={columns}
           data={data}
           handleAdd={handleAdd}
-          renderRowSubComponent={renderRowSubComponent}
           getHeaderProps={(column: HeaderGroup) => column.getSortByToggleProps()}
         />
       </ScrollX>
