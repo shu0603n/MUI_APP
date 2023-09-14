a3628506dacf36e878a69f9b3e016c375074d6b5
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-14 11:25:32 +0900

    Update employee-detail.tsx

--- src/pages/employee/employee-detail.tsx
+++ src/pages/employee/employee-detail.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useLocation, Link, Outlet } from 'react-router-dom';
+import { useLocation, Link, Outlet, useParams } from 'react-router-dom';
 
 // material-ui
 import { Box, Tab, Tabs } from '@mui/material';
@@ -14,25 +14,26 @@ import { ContainerOutlined, FileTextOutlined, LockOutlined, SettingOutlined, Tea
 
 const EmployeeDetail = () => {
   const { pathname } = useLocation();
+  const { id } = useParams();
 
   let selectedTab = 0;
   switch (pathname) {
-    case '/employee/employee-detail/personal':
+    case `/employee/detail/${id}/personal`:
       selectedTab = 1;
       break;
-    case '/employee/employee-detail/my-account':
+    case `/employee/detail/${id}/my-account`:
       selectedTab = 2;
       break;
-    case '/employee/employee-detail/password':
+    case `/employee/detail/${id}/password`:
       selectedTab = 3;
       break;
-    case '/employee/employee-detail/skill':
+    case `/employee/detail/${id}/skill`:
       selectedTab = 4;
       break;
-    case '/employee/employee-detail/settings':
+    case `/employee/detail/${id}/settings`:
       selectedTab = 5;
       break;
-    case '/employee/employee-detail/basic':
+    case `/employee/detail/${id}/basic`:
     default:
       selectedTab = 0;
   }
@@ -47,24 +48,24 @@ const EmployeeDetail = () => {
     <MainCard border={false} boxShadow>
       <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%' }}>
         <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="auto" aria-label="account profile tab">
-          <Tab label="プロフィール" component={Link} to="/employee/employee-detail/basic" icon={<UserOutlined />} iconPosition="start" />
-          <Tab label="個人情報" component={Link} to="/employee/employee-detail/personal" icon={<FileTextOutlined />} iconPosition="start" />
-          <Tab label="スキル一覧" component={Link} to="/employee/employee-detail/skill" icon={<TeamOutlined />} iconPosition="start" />
+          <Tab label="プロフィール" component={Link} to={`/employee/detail/${id}/basic`} icon={<UserOutlined />} iconPosition="start" />
+          <Tab label="個人情報" component={Link} to={`/employee/detail/${id}/personal`} icon={<FileTextOutlined />} iconPosition="start" />
+          <Tab label="スキル一覧" component={Link} to={`/employee/detail/${id}/skill`} icon={<TeamOutlined />} iconPosition="start" />
           <Tab
             label="請求一覧"
             component={Link}
-            to="/employee/employee-detail/my-account"
+            to={`/employee/detail/${id}/my-account`}
             icon={<ContainerOutlined />}
             iconPosition="start"
           />
           <Tab
             label="パスワード変更"
             component={Link}
-            to="/employee/employee-detail/password"
+            to={`/employee/detail/${id}/password`}
             icon={<LockOutlined />}
             iconPosition="start"
           />
-          <Tab label="設定" component={Link} to="/employee/employee-detail/settings" icon={<SettingOutlined />} iconPosition="start" />
+          <Tab label="設定" component={Link} to={`/employee/detail/${id}/settings`} icon={<SettingOutlined />} iconPosition="start" />
         </Tabs>
       </Box>
       <Box sx={{ mt: 2.5 }}>
