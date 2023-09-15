a136e6b3079acb5857456081e2bc5d94dab1516d
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-15 10:34:48 +0900

    Update employee.ts

--- src/mock/employee/employee.ts
+++ src/mock/employee/employee.ts
@@ -1,5 +1,6 @@
+import { MockEmployment } from 'mock/param/employment';
+import { MockEmployeeSkillHistory, MockJobCategory, MockPosition3, MockProject } from 'mock/param/skill';
 import { Employee } from 'types/employee/employee';
-import { MockEmployeeSkill } from './employee_skill';
 
 export const MockEmployee: Employee = {
   employee_id: 1, //社員ID
@@ -9,17 +10,17 @@ export const MockEmployee: Employee = {
   first_name_k: 'シュンスケ', //名カナ
   gender: '男', //性別
   birthday: '1994/06/03', //生年月日
-  job_category: 'SE', //職種
+  job_category: MockJobCategory.job_category_name, //職種
   client_name: '株式会社テクノプロ', //顧客ID
-  project_name: 'フロントエンド改修の案件', //案件
+  project_name: MockProject.project_title, //案件
   address: '北海道札幌市豊平区', //住所
   joining_date: '1994/06/03', //入社日
   retirement_date: '1994/06/03', //退職日
   phone_number: '09095219336', //電話番号
   email_address: 'shu0603n@gmail.com', //メールアドレス
-  employee_skills: [MockEmployeeSkill], //社員スキルID
+  employee_skills: MockEmployeeSkillHistory, //社員スキルID
   postal_code: '0620907', //郵便番号
   remarks: 'ストレスに弱い', //備考
-  position_name: '課長', //役職ID
-  employment_name: '正社員' //雇用ID
+  position_name: MockPosition3.position_name, //役職ID
+  employment_name: MockEmployment.employment_name //雇用ID
 };
