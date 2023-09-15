9d3fd13d875152190af3805d57e58876214b5f58
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-15 12:33:11 +0900

    Update employee_skill.ts

--- src/mock/employee/employee_skill.ts
+++ src/mock/employee/employee_skill.ts
@@ -1,14 +1,21 @@
 import { MockProcess, MockProcess2, MockProcess3, MockProcess4 } from 'mock/param/process';
-import { MockEmployeeSkillHistory } from 'mock/param/skill';
-import { EmployeeSkill } from 'types/employee/employee_skill';
+import { MockProject, MockProject2 } from 'mock/param/skill';
+import { EmployeeSkillHistory } from 'types/employee/employee_skill';
 
-export const MockEmployeeSkill: EmployeeSkill = {
-  employee_skills: MockEmployeeSkillHistory.skills, //社員スキル
+export const MockEmployeeSkill: EmployeeSkillHistory = {
   experienct_years: 4, //経験年数
-  project_title: '顧客管理システムの修正', //プロジェクト名
+  project: MockProject, //プロジェクト名
   people_number: 8, //プロジェクト規模
-  cliant: '株式会社テクノプロ', //顧客ID
+  contact: 'PG',
   process_list: [MockProcess.process_name, MockProcess2.process_name, MockProcess3.process_name, MockProcess4.process_name] //工程ID
 };
 
-export const MockEmployeeSkillAll: EmployeeSkill[] = [MockEmployeeSkill];
+export const MockEmployeeSkill2: EmployeeSkillHistory = {
+  experienct_years: 1, //経験年数
+  project: MockProject2, //プロジェクト名
+  people_number: 3, //プロジェクト規模
+  contact: 'PG',
+  process_list: [MockProcess.process_name, MockProcess2.process_name, MockProcess3.process_name, MockProcess4.process_name] //工程ID
+};
+
+export const MockEmployeeSkillAll: EmployeeSkillHistory[] = [MockEmployeeSkill, MockEmployeeSkill2];
