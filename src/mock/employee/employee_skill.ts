90f8c8b2e7e2f3dfe7e5c5d6eb96895d218040fd
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-15 10:34:36 +0900

    Update employee_skill.ts

--- src/mock/employee/employee_skill.ts
+++ src/mock/employee/employee_skill.ts
@@ -1,13 +1,14 @@
-import { MockSkillArray } from 'mock/param/skill';
+import { MockProcess, MockProcess2, MockProcess3, MockProcess4 } from 'mock/param/process';
+import { MockEmployeeSkillHistory } from 'mock/param/skill';
 import { EmployeeSkill } from 'types/employee/employee_skill';
 
 export const MockEmployeeSkill: EmployeeSkill = {
-  employee_skills: MockSkillArray, //社員スキル
+  employee_skills: MockEmployeeSkillHistory.skills, //社員スキル
   experienct_years: 4, //経験年数
   project_title: '顧客管理システムの修正', //プロジェクト名
   people_number: 8, //プロジェクト規模
   cliant: '株式会社テクノプロ', //顧客ID
-  process_list: ['基本設計', '詳細設計'] //工程ID
+  process_list: [MockProcess.process_name, MockProcess2.process_name, MockProcess3.process_name, MockProcess4.process_name] //工程ID
 };
 
-export const MockEmployeeSkillArray: EmployeeSkill[] = [MockEmployeeSkill];
+export const MockEmployeeSkillAll: EmployeeSkill[] = [MockEmployeeSkill];
