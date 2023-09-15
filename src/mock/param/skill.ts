20ef8ec96622cebb002372b811516c384a6e0cf0
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-15 10:33:46 +0900

    Update skill.ts

--- src/mock/param/skill.ts
+++ src/mock/param/skill.ts
@@ -1,12 +1,139 @@
-import { Skill } from 'types/param/skill';
+import {
+  Contract,
+  EmployeeSkillHistory,
+  JobCategory,
+  Position,
+  Project,
+  ProjectSkillHistory,
+  ProjectType,
+  Skill,
+  Technic,
+  Working
+} from 'types/param/skill';
+
+export const MockWorking: Working = {
+  working_status: '勤務中' //勤務名
+};
+export const MockWorking2: Working = {
+  working_status: '退勤' //勤務名
+};
+
+export const MockWorking3: Working = {
+  working_status: '予定休' //勤務名
+};
+export const MockWorking4: Working = {
+  working_status: '当日休' //勤務名
+};
+
+export const MockJobCategory: JobCategory = {
+  job_category_name: 'SE' //職種名
+};
+export const MockJobCategory2: JobCategory = {
+  job_category_name: 'CC' //職種名
+};
+export const MockJobCategory3: JobCategory = {
+  job_category_name: 'TA' //職種名
+};
+export const MockJobCategory4: JobCategory = {
+  job_category_name: '事務' //職種名
+};
+
+export const MockProjectType: ProjectType = {
+  project_type_name: 'WBアプリケーション' //種別名
+};
+export const MockProjectType2: ProjectType = {
+  project_type_name: '顧客管理システム' //種別名
+};
+export const MockProjectType3: ProjectType = {
+  project_type_name: 'HP' //種別名
+};
+export const MockProjectType4: ProjectType = {
+  project_type_name: 'コンソール' //種別名
+};
+
+export const MockPosition: Position = {
+  position_name: '支店長' //役職名
+};
+export const MockPosition2: Position = {
+  position_name: '副支店長' //役職名
+};
+export const MockPosition3: Position = {
+  position_name: 'マネージャー' //役職名
+};
+export const MockPosition4: Position = {
+  position_name: 'エリアマネージャー' //役職名
+};
+export const MockPosition5: Position = {
+  position_name: 'なし' //役職名
+};
+
+export const MockTechnic: Technic = {
+  technic_name: '言語' //技術名
+};
+export const MockTechnic2: Technic = {
+  technic_name: 'ライブラリ' //技術名
+};
+export const MockTechnic3: Technic = {
+  technic_name: 'DataBase' //技術名
+};
+export const MockTechnic4: Technic = {
+  technic_name: 'Server' //技術名
+};
+export const MockTechnic5: Technic = {
+  technic_name: 'ツール' //技術名
+};
+export const MockTechnic6: Technic = {
+  technic_name: 'OS' //技術名
+};
+export const MockContract: Contract = {
+  contract_name: '派遣' //契約区分名
+};
+export const MockContract2: Contract = {
+  contract_name: '紹介予定派遣' //契約区分名
+};
+export const MockContract3: Contract = {
+  contract_name: '業務委託' //契約区分名
+};
+export const MockContract4: Contract = {
+  contract_name: '準委任契約' //契約区分名
+};
+export const MockContract5: Contract = {
+  contract_name: '受託開発' //契約区分名
+};
+export const MockContract6: Contract = {
+  contract_name: '自社開発' //契約区分名
+};
 
 export const MockSkill: Skill = {
-  technic: '言語', //技術区分ID
+  technic: MockTechnic.technic_name, //技術区分ID
   skill_name: 'JAVA' //スキル名
 };
 export const MockSkill2: Skill = {
-  technic: '言語', //技術区分ID
+  technic: MockTechnic.technic_name, //技術区分ID
   skill_name: 'Python' //スキル名
 };
 
-export const MockSkillArray = [MockSkill, MockSkill2];
+export const MockSkillAll = [MockSkill, MockSkill2];
+
+export const MockEmployeeSkillHistory: EmployeeSkillHistory = {
+  skills: [MockSkill, MockSkill2]
+};
+export const MockProjectSkill: ProjectSkillHistory = {
+  //[案件]
+  skills: [MockSkill, MockSkill2]
+};
+
+export const MockProject: Project = {
+  //[案件]
+  client_name: '株式会社テクノプロ', //顧客ID
+  project_skills: MockProjectSkill.skills, //案件スキルID
+  hp_posting_flag: true, //HP掲載フラグ
+  contract_name: MockContract.contract_name, //契約区分ID
+  working_postal_code: '0010005', //就業先郵便番号
+  working_address: '北海道札幌市中央区1-5', //就業先住所
+  working_start_time: '9:00', //就業開始時刻
+  working_end_time: '18:00', //就業終了時刻
+  holiday: '土日祝', //休日
+  project_title: 'フロントエンド改修', //案件名
+  business_outline: 'Reactを使用し、某メディア媒体のテスト環境を作成する' //業務内容
+};
