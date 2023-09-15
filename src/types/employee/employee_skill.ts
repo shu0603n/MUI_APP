66d77482afd5715dd5e1ec5a7df183a9d5c9b02f
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-15 12:34:46 +0900

    Update employee_skill.ts

--- src/types/employee/employee_skill.ts
+++ src/types/employee/employee_skill.ts
@@ -1,10 +1,9 @@
-import { Skill } from 'types/param/skill';
+import { Project } from 'types/param/skill';
 
-export type EmployeeSkill = {
-  employee_skills: Skill[]; //社員スキルID
+export type EmployeeSkillHistory = {
   experienct_years: number; //経験年数
-  project_title: string; //プロジェクト名
+  project: Project; //プロジェクト名
   people_number: number; //プロジェクト規模
-  cliant: string; //顧客ID
+  contact: string;
   process_list: string[]; //工程履歴ID
 };
